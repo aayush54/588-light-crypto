492c5b5b6afe7e25e3e31c64f084bf902000f4da
Bhatt <rnbhatt@UMICH.EDU>
2024-04-08 17:04:44 -0400

    Made sense of functions

--- src/test.cpp
+++ src/test.cpp
@@ -8,70 +8,62 @@ extern "C"{                 // we need this otherwise it can't find the function
 #include <array>
 #include <cassert>
 
-using ustring = std::basic_string<unsigned char>;
 
-inline const unsigned char* uc_str(const char* s) {
+inline const unsigned char* cuc_str(const char* s) {
     return reinterpret_cast<const unsigned char*>(s);
 }
 
+inline unsigned char* uc_str(char* s) {
+    return reinterpret_cast<unsigned char*>(s);
+}
 
-ustring encrypt(const ustring& input, const ustring& associatedData,
+
+
+std::string encrypt(const std::string& input, const std::string& associatedData,
     const std::array<unsigned char, CRYPTO_NPUBBYTES>& nonce,
     const std::array<unsigned char, CRYPTO_KEYBYTES>& key
     ) {
 
     unsigned long long clen = CRYPTO_ABYTES;
 
-    ustring encrypted;
+    std::string encrypted;
     encrypted.resize(input.size() * 2);
 
-    crypto_aead_encrypt(encrypted.data(), &clen, input.data(), input.size(),
-        associatedData.data(), associatedData.size(), (const unsigned char*)0,
+    crypto_aead_encrypt(uc_str(encrypted.data()), &clen, cuc_str(input.data()), input.size(),
+        cuc_str(associatedData.data()), associatedData.size(), (const unsigned char*)0,
         nonce.data(), key.data());
     
+    encrypted.resize(clen);
 
     return encrypted;
 }
 
-ustring decrypt(const ustring& encrypted, const ustring& associatedData,
+std::string decrypt(const std::string& encrypted, const std::string& associatedData,
     const std::array<unsigned char, CRYPTO_NPUBBYTES>& nonce,
     const std::array<unsigned char, CRYPTO_KEYBYTES>& key
 ) {
 
-    ustring decrypted;
+    std::string decrypted;
     decrypted.resize(encrypted.size());
 
     auto outsize = decrypted.size();
 
-    crypto_aead_decrypt(decrypted.data(), &outsize, (unsigned char*)0, encrypted.data(), encrypted.size(),
-        associatedData.data(), associatedData.size(), nonce.data(), key.data());
+    crypto_aead_decrypt(uc_str(decrypted.data()), &outsize, (unsigned char*)0, cuc_str(encrypted.data()), encrypted.size(),
+        cuc_str(associatedData.data()), associatedData.size(), nonce.data(), key.data());
 
     return decrypted;
 }
 
 int main() {
 
-    unsigned char n[CRYPTO_NPUBBYTES] = { 0, 1, 2,  3,  4,  5,  6,  7,
-                                      8, 9, 10, 11, 12, 13, 14, 15 };
-    unsigned char k[CRYPTO_KEYBYTES] = { 0, 1, 2,  3,  4,  5,  6,  7,
-                                        8, 9, 10, 11, 12, 13, 14, 15 };
-    unsigned char a[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-    unsigned char m[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-    unsigned char c[32], h[32], t[32];
-    unsigned long long alen = 16;
-    unsigned long long mlen = 16;
-    unsigned long long clen = CRYPTO_ABYTES;
-    int result = 0;
-
-    result |= crypto_aead_encrypt(c, &clen, m, mlen, a, alen, (const unsigned char*)0, n, k);
-    result |= crypto_aead_decrypt(m, &mlen, (unsigned char*)0, c, clen, a, alen, n, k);
-
-    ustring associatedData{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    std::string associatedData{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
     std::array<unsigned char, CRYPTO_NPUBBYTES> nonce{ 0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15 };
     std::array<unsigned char, CRYPTO_KEYBYTES> key{ 0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-    auto encrypted = encrypt(uc_str("hello"), associatedData,nonce,key);
+    auto encrypted = encrypt("hello", associatedData,nonce,key);
     auto decrypted = decrypt(encrypted, associatedData, nonce, key);
 
-    int x = 0;
+    std::cout << "encrypted size: " << encrypted.size() << std::endl;
+    std::cout << decrypted << std::endl;
+
 }
