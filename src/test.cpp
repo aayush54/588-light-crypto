6879ba27e609f11e76bbcf8bd1c9801b72864ddc
Bhatt <rnbhatt@UMICH.EDU>
2024-04-08 17:09:55 -0400

    Fix a bug

--- src/test.cpp
+++ src/test.cpp
@@ -18,16 +18,15 @@ inline unsigned char* uc_str(char* s) {
 }
 
 
-
-std::string encrypt(const std::string& input, const std::string& associatedData,
+std::string ascon_encrypt(const std::string& input, const std::string& associatedData,
     const std::array<unsigned char, CRYPTO_NPUBBYTES>& nonce,
     const std::array<unsigned char, CRYPTO_KEYBYTES>& key
     ) {
 
-    unsigned long long clen = CRYPTO_ABYTES;
+    unsigned long long clen = input.size() + CRYPTO_ABYTES;
 
     std::string encrypted;
-    encrypted.resize(input.size() * 2);
+    encrypted.resize(clen);
 
     crypto_aead_encrypt(uc_str(encrypted.data()), &clen, cuc_str(input.data()), input.size(),
         cuc_str(associatedData.data()), associatedData.size(), (const unsigned char*)0,
@@ -38,7 +37,7 @@ std::string encrypt(const std::string& input, const std::string& associatedData,
     return encrypted;
 }
 
-std::string decrypt(const std::string& encrypted, const std::string& associatedData,
+std::string ascon_decrypt(const std::string& encrypted, const std::string& associatedData,
     const std::array<unsigned char, CRYPTO_NPUBBYTES>& nonce,
     const std::array<unsigned char, CRYPTO_KEYBYTES>& key
 ) {
@@ -60,8 +59,8 @@ int main() {
     std::array<unsigned char, CRYPTO_NPUBBYTES> nonce{ 0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15 };
     std::array<unsigned char, CRYPTO_KEYBYTES> key{ 0, 1, 2,  3,  4,  5,  6,  7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-    auto encrypted = encrypt("hello", associatedData,nonce,key);
-    auto decrypted = decrypt(encrypted, associatedData, nonce, key);
+    auto encrypted = ascon_encrypt("hello", associatedData,nonce,key);
+    auto decrypted = ascon_decrypt(encrypted, associatedData, nonce, key);
 
     std::cout << "encrypted size: " << encrypted.size() << std::endl;
     std::cout << decrypted << std::endl;
