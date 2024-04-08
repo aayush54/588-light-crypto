76fa17e751a9d530236ff30f8c9c0866863fb9f7
Bhatt <rnbhatt@UMICH.EDU>
2024-04-08 17:36:03 -0400

    Port a stackoverflow example -- will refactor later

--- src/test.cpp
+++ src/test.cpp
@@ -59,6 +59,143 @@ std::string ascon_decrypt(const std::string& encrypted, const std::string& assoc
     return decrypted;
 }
 
+// OpenSSL code adapted from: https://stackoverflow.com/questions/9889492/how-to-do-encryption-using-aes-in-openssl
+void ssl_handleErrors(void)
+{
+    unsigned long errCode;
+
+    printf("An error occurred\n");
+    while (errCode = ERR_get_error())
+    {
+        char* err = ERR_error_string(errCode, NULL);
+        printf("%s\n", err);
+    }
+    abort();
+}
+
+int ssl_encrypt(unsigned char* plaintext, int plaintext_len, unsigned char* aad,
+    int aad_len, unsigned char* key, unsigned char* iv,
+    unsigned char* ciphertext, unsigned char* tag)
+{
+    EVP_CIPHER_CTX* ctx = NULL;
+    int len = 0, ciphertext_len = 0;
+
+    /* Create and initialise the context */
+    if (!(ctx = EVP_CIPHER_CTX_new())) ssl_handleErrors();
+
+    /* Initialise the encryption operation. */
+    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
+        ssl_handleErrors();
+
+    /* Set IV length if default 12 bytes (96 bits) is not appropriate */
+    if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 16, NULL))
+        ssl_handleErrors();
+
+    /* Initialise key and IV */
+    if (1 != EVP_EncryptInit_ex(ctx, NULL, NULL, key, iv)) ssl_handleErrors();
+
+    /* Provide any AAD data. This can be called zero or more times as
+     * required
+     */
+    if (aad && aad_len > 0)
+    {
+        if (1 != EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len))
+            ssl_handleErrors();
+    }
+
+    /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if (plaintext)
+    {
+        if (1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+            ssl_handleErrors();
+
+        ciphertext_len = len;
+    }
+
+    /* Finalise the encryption. Normally ciphertext bytes may be written at
+     * this stage, but this does not occur in GCM mode
+     */
+    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) ssl_handleErrors();
+    ciphertext_len += len;
+
+    /* Get the tag */
+    if (1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag))
+        ssl_handleErrors();
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
+
+int ssl_decrypt(unsigned char* ciphertext, int ciphertext_len, unsigned char* aad,
+    int aad_len, unsigned char* tag, unsigned char* key, unsigned char* iv,
+    unsigned char* plaintext)
+{
+    EVP_CIPHER_CTX* ctx = NULL;
+    int len = 0, plaintext_len = 0, ret;
+
+    /* Create and initialise the context */
+    if (!(ctx = EVP_CIPHER_CTX_new())) ssl_handleErrors();
+
+    /* Initialise the decryption operation. */
+    if (!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, NULL, NULL))
+        ssl_handleErrors();
+
+    /* Set IV length. Not necessary if this is 12 bytes (96 bits) */
+    if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, 16, NULL))
+        ssl_handleErrors();
+
+    /* Initialise key and IV */
+    if (!EVP_DecryptInit_ex(ctx, NULL, NULL, key, iv)) ssl_handleErrors();
+
+    /* Provide any AAD data. This can be called zero or more times as
+     * required
+     */
+    if (aad && aad_len > 0)
+    {
+        if (!EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len))
+            ssl_handleErrors();
+    }
+
+    /* Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary
+     */
+    if (ciphertext)
+    {
+        if (!EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+            ssl_handleErrors();
+
+        plaintext_len = len;
+    }
+
+    /* Set expected tag value. Works in OpenSSL 1.0.1d and later */
+    if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, tag))
+        ssl_handleErrors();
+
+    /* Finalise the decryption. A positive return value indicates success,
+     * anything else is a failure - the plaintext is not trustworthy.
+     */
+    ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    if (ret > 0)
+    {
+        /* Success */
+        plaintext_len += len;
+        return plaintext_len;
+    }
+    else
+    {
+        /* Verify failed */
+        return -1;
+    }
+}
+
 int main() {
 
     // openssl stuff that needs to happen before you do anything
